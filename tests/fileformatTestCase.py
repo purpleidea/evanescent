609111fee3f6873e103dd2dd922a679aa400a220
james shubin <purpleidea@gmail.com>
2009-11-18 11:07:05 -0500

    file format test case now takes care of licenses and supports constraints. perhaps there is a better vocabulary for the constraints. needs a bit of polish perhaps.

--- tests/fileformatTestCase.py
+++ tests/fileformatTestCase.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""tests.modelTestCase
+"""tests.fileformatTestCase
 
-Model test case for adding tests into the automatic suite.
-To add a test case:
-	1) copy this file to: <xyzTestCase.py> (where `xyz' is the new name)
-	2) rename the class to <xyzTestCase> (where `xyz' is the new name)
-	3) modify methods in the class, building your test case
-	4) edit this docstring to show the correct name and short description
-	5) test by running this file. will automatically run with main suite
+Test that all the files in our code distribution match a common set of aspects
+and constraints. This generally tests the physical layout of the code. While
+this may seem annoying, it provides an automatic lookout for small errors and
+discrepancies that keep the code from otherwise being uniform and polished.
 """
 
 # Copyright (C) 2009  James Shubin, McGill University
@@ -31,42 +28,186 @@ import os
 import sys
 import unittest
 
-# do some path magic so this can run anyhow from anywhere, and some magic names
-if __name__ == '__main__':
-	__testname = os.path.splitext(__file__)[0]
-	if __testname.startswith('./'):
-		__testname = __testname.partition('./')[2]
-	sys.path.append('../src/')
-else:
-	__testname = __name__[__name__.rfind('.')+1:]
-	sys.path.append('src/')
+_ = lambda x: x			# add fake gettext function until i fix up i18n
 
-__all__ = [__testname]
+VERBOSE = False
+EXCLUDED = ['.git', 'play', 'old', 'wotd', 'tar']	# dirs to exclude
+MAXSIZE = 1*1024*1024	# 1 MiB (don't let read() eat up all my memory!)
 
-class modelTestCase(unittest.TestCase):
+# order in which the elements should be verified.
+ORDERING = ['SHEBANG', 'CODING', 'LICENSE', 'COPYRIGHT']
+
+# set of elements that we want to match
+ELEMENTS = {
+	'SHEBANG': '#!/usr/bin/python',
+	'CODING': '# -*- coding: utf-8 -*-',
+	'LICENSE': '''
+	# This program is free software: you can redistribute it and/or modify
+	# it under the terms of the GNU Affero General Public License as published by
+	# the Free Software Foundation, either version 3 of the License, or
+	# (at your option) any later version.
+	#
+	# This program is distributed in the hope that it will be useful,
+	# but WITHOUT ANY WARRANTY; without even the implied warranty of
+	# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	# GNU Affero General Public License for more details.
+	#
+	# You should have received a copy of the GNU Affero General Public License
+	# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	'''.replace('\t', ''),	# replace tabs used to align this text with code
+
+	# TODO: how do we intelligently match and check the year/year range?
+	# TODO: should we use fnmatch or regex or a custom match ?
+	'COPYRIGHT': '''
+	# Copyright (C) 2009  James Shubin, McGill University
+	# Written for McGill University by James Shubin <purpleidea@gmail.com>
+	#
+	'''.replace('\t', ''),
+}
+
+# NOTE: line numbers for constraints are 1-based (like my text editor)
+CONSTRAINTS = {
+	'SHEBANG': lambda l: l['SHEBANG'] == 1,
+	'CODING': lambda l: l['CODING'] == l['SHEBANG']+1,
+	'LICENSE': lambda l: l['LICENSE'] > l['CODING']
+}
+
+# simple check and setup on ELEMENTS and LOCATION.
+for i in ORDERING:
+	if i not in ELEMENTS:
+		raise AssertionError(_('Element: %s, is missing in ELEMENTS.') % i)
+
+# do some path magic so this can run anyhow from anywhere
+TESTNAME = os.path.splitext(os.path.basename(os.path.normpath(__file__)))[0]
+BASEPATH = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
+sys.path.append(os.path.join(BASEPATH, 'src/'))
+__all__ = [TESTNAME]
+
+
+# TODO: this test case should check the format for all .py files... DO THIS.
+# TODO: maybe replace the licenseTestCase.py file with this.
+class fileformatTestCase(unittest.TestCase):
 	"""test the dt class."""
 
 	def setUp(self):
 		"""setup code that has to occur for each test*()"""
-		import datetime
-		import dt
-		stamp = datetime.datetime(1946, 5, 11, 3+12, 14, 15)
-		self.obj = dt.dt(datetime=stamp)
 
-	def testSimple1(self):
-		self.failUnless(self.obj.is_time('[15, 16]'), 'hour range fails')
+		self.filelist = []
+		#dirname = dirname.rstrip('/')
+		#if self.__isexcluded(dirname):
+		#	return
 
-	def testSimple2(self):
-		self.failUnless(self.obj.is_time('[15:00 , 15:15['), 'hour and minute range fails')
+		for (root, dirs, files) in os.walk(BASEPATH):
 
-	def testSimple3(self):
-		self.failUnless(self.obj.is_time('[8:00:00 , 15:14:20['), 'hour, minute and second range fails')
+			remove = []
+			for x in dirs:
+				#XXX print 'is?: %s' % x
+				if self.__isexcluded(x):
+					# keep track of ones we want to exclude
+					remove.append(x)
 
-	def testBoundary1(self):
-		self.failUnless(self.obj.is_time('[15:14:15 , 15:15:16]'), 'time doesn\'t match range 3')
+			# remove all the bad ones here so as not to disrupt loop
+			for x in remove:
+				dirs.remove(x)
+
+			# TODO: add exclusions ?
+			for x in files:
+				if os.path.splitext(x)[1] == '.py':
+					self.filelist.append(os.path.join(root, x))
+
+
+	def __isexcluded(self, dirname):
+		"""should a directory be excluded ?"""
+		# TODO: use: fnmatch.fnmatchcase ?
+		for i in EXCLUDED:
+			if dirname == i or dirname.endswith(i):
+				#XXX print 'X %s' % dirname
+				return True
+		#XXX print 'Y %s' % dirname
+		return False
+
+
+	def checkFile(self, filename):
+		"""checks a particular file name against all the available
+		tests."""
+		with open(filename, 'r') as f:
+			data = f.read(MAXSIZE)
+
+		def i2l(index, data=data):
+			"""given a string; usually the output from file.read()
+			and a character index, returns which the index was on.
+			this function returns a 1-based number like my vim."""
+			index = abs(index)
+			if index >= len(data): return -1
+			return len(data[0:index].split('\n'))
+
+		location = {}
+		# initialize dictionary
+		for key in ORDERING: location.setdefault(i, -1)
+
+		for key in ORDERING:
+			try:
+				# find the particular element inside of data
+				location[key] = data.index(ELEMENTS[key])
+			except ValueError, e:
+				print _('%s was not found in: %s') % (key, filename)
+				# XXX: would be nice to inspect constraint and
+				# see what should be in it's place.
+				return False
+
+			# dictionary comprehension (build dictionary of lines)
+			lines = dict([(k,i2l(v)) for k,v in location.items()])
+			# check constraint
+			if not CONSTRAINTS.get(key, lambda _: True)(lines):
+			#if key in CONSTRAINTS and not(CONSTRAINTS[key](i2l(lines))):
+				print _('%s failed constraint in: %s') % (key, filename)
+				# XXX: would be nice to display constraint here
+				print _('location table is: %s') % location
+				print _('line number table is: %s') % lines
+				return False
+
+		return True
+
+
+	def checkFiles(self, files, short_circuit=False):
+		"""runs checkFile() on a list of files and returns an
+		accumulated success or failure status."""
+
+		result = True
+		for i in files:
+			if not self.checkFile(i):
+				result = False
+				if short_circuit: break
+
+		return result
+
+
+	def testFiles(self):
+		"""test suite hook, that calls checkFiles(). this function
+		depends on the setUp() function."""
+		self.failUnless(
+			self.checkFiles(self.filelist),
+			_('Files don\'t pass format checks and constraints.')
+		)
+
+
+	def XXXtestSheBang(self):
+		failed = 0
+		for i in self.filelist:
+			with open(i, 'r') as f:
+				line = f.readlines(1)[0][:-1]
+				if line != ELEMENTS['SHEBANG']:
+					failed = failed + 1
+					print 'shebang fails at: %s' % i
+					print 'shebang shown is: %s' % line
+					print 'shebang model is: %s' % ELEMENTS['SHEBANG']
+
+		if VERBOSE: print ''
+		self.failUnless(failed == 0, 'one or more files failed shebang')
+
 
 # group all tests into a suite
-suite = unittest.TestLoader().loadTestsFromTestCase(globals()[__testname])
+suite = unittest.TestLoader().loadTestsFromTestCase(globals()[TESTNAME])
 
 # if this file is run individually, then run these test cases
 if __name__ == '__main__':
