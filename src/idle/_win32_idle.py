9929f91a8192d952630090cef2cd8b37eadd8070
James Shubin <purpleidea@gmail.com>
2010-05-05 11:43:52 -0400

    Small formatting fixes.

--- src/idle/_win32_idle.py
+++ src/idle/_win32_idle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Idle time detection for Windows.
+"""Idle time detection for Windows.
 
 Returns idle time on Windows. Meant to be called by a higher level wrapper.
 """
@@ -25,7 +24,7 @@ Returns idle time on Windows. Meant to be called by a higher level wrapper.
 
 import os
 if os.name != 'nt':
-	raise ImportError("This modules requires Windows 2000 or newer.")
+	raise ImportError('This modules requires Windows 2000 or newer.')
 import ctypes
 
 __all__ = ['_idle']
@@ -34,7 +33,7 @@ _GetTickCount = ctypes.windll.kernel32.GetTickCount
 _GetLastInputInfo = ctypes.windll.user32.GetLastInputInfo
 
 class _LastInputInfo(ctypes.Structure):
-	_fields_ = [("cbSize", ctypes.c_uint), ("dwTime", ctypes.c_uint32)]
+	_fields_ = [('cbSize', ctypes.c_uint), ('dwTime', ctypes.c_uint32)]
 
 def _idle():
 	"""returns the number of milliseconds a user is idle."""
@@ -42,7 +41,7 @@ def _idle():
 	inputinfo.cbSize = ctypes.sizeof(inputinfo)
 
 	if not _GetLastInputInfo(ctypes.byref(inputinfo)):
-		raise OSError("GetLastInputInfo failed")
+		raise OSError('GetLastInputInfo failed')
 
 	# number of idle milliseconds:
 	return (_GetTickCount() - inputinfo.dwTime)
