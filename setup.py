df5ad6570ac7e6567d5cff1f07a791ccb48d3faf
James Shubin <purpleidea@gmail.com>
2010-05-11 11:06:20 -0400

    Rewrite of setup.py file.

--- setup.py
+++ setup.py
@@ -31,45 +31,56 @@ from jhelp import build_manpages
 from src import prefix		# used to find the name of this project
 
 # VARIABLES ###################################################################
-NAME = 'evanescent'
-ext_modules = []
-data_files = []
+NAME = prefix.name('src')		# should be the name of this dir
+# this pulls the one-line description and long description from the docstring
+DESCRIPTION, LDESCRIPTION = pydoc.splitdoc(pydoc.getdoc(sys.modules[__name__]))
 
-# EXTENSIONS ##################################################################
-if os.name == 'posix':
-	ext_modules.append(
-		distutils.core.Extension('evanescent.idle._x11_idle', ['src/idle/_x11_idle.c'], libraries=['Xss'])
-	)
-
-# DATA FILES ##################################################################
-data_files.append(('share/%s' % NAME, ['files/evanescent.png']))
-data_files.append(('share/%s' % NAME, ['files/evanescent.svg']))
-data_files.append(('share/%s' % NAME, ['COPYING']))
-data_files.append(('share/%s' % NAME, ['AUTHORS']))
-data_files.append(('share/%s' % NAME, ['VERSION']))
 # see: http://standards.freedesktop.org/autostart-spec/autostart-spec-latest.html
 # and: http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
-if os.name == 'posix':
-	data_files.append(('/etc/xdg/autostart/', ['files/evanescent.desktop']))
-	data_files.append(('/etc/event.d/', ['files/evanescent.upstart']))
-	data_files.append(('share/dbus-1/services/', ['files/ca.mcgill.cs.dazzle.evanescent.client.service']))
-	data_files.append(('share/doc/%s/' % NAME, ['files/evanescent.conf.yaml.example']))
-	data_files.append(('share/man/man1/', ['man/evanescent.1.gz']))
-	data_files.append(('share/man/man1/', ['files/evanescent-daemon.1.gz']))
-	data_files.append(('share/man/man1/', ['files/evanescent-client.1.gz']))
-	data_files.append(('share/man/man1/', ['files/evanescent-remote.1.gz']))
-	data_files.append(('share/man/man1/', ['files/evanescent-config.1.gz']))
 
 # SETUP #######################################################################
 distutils.core.setup(
 	name=NAME,
 	version=misc.get_version(),
+	author='James Shubin',
+	author_email='purpleidea@gmail.com',
+	url='http://www.cs.mcgill.ca/~james/code/',
+	description=DESCRIPTION,
+	long_description=LDESCRIPTION,
+	# http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	classifiers=[
+		'Environment :: X11 Applications :: GTK',
+		'Environment :: X11 Applications :: GTK',
+		'Intended Audience :: System Administrators',
+		'License :: OSI Approved :: GNU Affero General Public License v3',
+		'Topic :: System :: Systems Administration',
+	],
 	packages=['evanescent', 'evanescent.idle', 'evanescent.logout'],
-	package_dir={'evanescent': 'src'},
-	ext_modules=ext_modules,
-	data_files=data_files,
+	package_dir={NAME: 'src'},
+	ext_modules=[
+		distutils.core.Extension('evanescent.idle._x11_idle', ['src/idle/_x11_idle.c'], libraries=['Xss']),
+		],
+	data_files=[
+		('share/%s' % NAME, ['Makefile']),
+		('share/%s' % NAME, misc.get_capitalized_files()),
+		('/etc/xdg/autostart/', ['files/evanescent.desktop']),
+		('/etc/event.d/', ['files/evanescent.upstart']),
+		# XXX: i bet that dbus doesn't look in /usr/local/share/dbus-1/
+		('/usr/share/dbus-1/services/', ['files/ca.mcgill.cs.dazzle.evanescent.client.service']),
+		('share/doc/%s/' % NAME, ['files/evanescent.conf.yaml.example']),
+		# images
+		('share/%s' % NAME, ['files/evanescent.png']),
+		('share/%s' % NAME, ['files/evanescent.svg']),
+		# man page related
+		('share/%s' % NAME, ['files/evanescent.1.template']),
+		('share/man/man1/', ['man/evanescent.1.gz']),
+		# FIXME: these should all be copies of the main file
+		#('share/man/man1/', ['files/evanescent-daemon.1.gz']),
+		#('share/man/man1/', ['files/evanescent-client.1.gz']),
+		#('share/man/man1/', ['files/evanescent-remote.1.gz']),
+		#('share/man/man1/', ['files/evanescent-config.1.gz']),
+	],
 	scripts=['evanescent-client', 'evanescent-daemon', 'evanescent-remote'],
-	# add build_manpages command
 	cmdclass={
 		'install': uninstall.install, 'uninstall': uninstall.uninstall,
 		'build_manpages': build_manpages.build_manpages,
